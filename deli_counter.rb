7be855b35a1f3c1cfb13d30fa32718a15169eb00
dynamic-distribution-2838 <harshrpatel93@gmail.com>
2019-03-08 20:53:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,5 +23,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts "There is nobody waiting to be served!" if katz_deli.length == 0
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!" 
+  else
+    puts "Currently seving #{katz_deli[0]}"
+    katz_deli.shift
+  end
 end
