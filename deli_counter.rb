7bdf9c39aacb3081413659d9a392c0112cb3aadd
dynamic-distribution-2838 <harshrpatel93@gmail.com>
2019-03-08 20:26:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+
+def line(array)
+  que = 'The line is currently: '
+  i = 1
+  while i < array.length
+    que += i + array[i]
+    i += 1
+  end
+  if array.length == 0 
+    puts "The line is currently empty."
+  else
+    puts que
+  end
+  
+end
